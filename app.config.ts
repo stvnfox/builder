a9b74a9f85fc1737949b3a5dcaf3ecc5aa78e8be
steven.vos <steven.vos@iodigital.com>
2025-03-23 17:15:22 +0100

    feat(tailwind): fix tailwind import

--- app.config.ts
+++ app.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from '@tanstack/react-start/config'
-import tsConfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from 'tailwindcss'
+import { defineConfig } from "@tanstack/react-start/config";
+import tsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   vite: {
     plugins: [
-      tsConfigPaths({
-        projects: ['./tsconfig.json'],
-      }),
       tailwindcss(),
+      tsConfigPaths({
+        projects: ["./tsconfig.json"],
+      }),
     ],
   },
-})
+});
