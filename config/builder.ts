cabf18c7de202a8075ab67357b786c2832ff02d6
steven.vos <steven.vos@iodigital.com>
2025-03-29 15:46:49 +0100

    refactor(config): add settings + hero block to config

--- config/builder.ts
+++ config/builder.ts
@@ -1,6 +1,12 @@
 import { HandMetal } from "lucide-react";
 
+import heroImage from "@/assets/header.svg?url";
+
 export const builderConfig = {
+	settings: {
+		showWaitingList: true,
+		waitingListLabel: "I want early access!",
+	},
 	navigation: {
 		title: "builder",
 		icon: HandMetal,
@@ -23,6 +29,19 @@ export const builderConfig = {
 				disabled: true,
 			},
 		],
-		showWaitingList: true,
+	},
+	hero: {
+		title: "Launch your new landing page in minutes!",
+		description:
+			"Introducing Builder—the intuitive platform that empowers you to create sleek, professional landing pages for your projects in minutes, not days. No coding skills required.",
+		image: {
+			url: heroImage,
+		},
+		callToAction: {
+			label: "Start building now!",
+			// TODO: Add the link to the signin page
+			href: "/",
+			openInNewTab: false,
+		},
 	},
 };
