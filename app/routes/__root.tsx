97575f74ed6277346ada6e718ecb025097a5853a
steven.vos <steven.vos@iodigital.com>
2025-03-29 11:05:25 +0100

    feat(toaster): add toaster and import css file differently for build purposes

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -4,8 +4,8 @@ import type { ConvexQueryClient } from "@convex-dev/react-query";
 import type { ConvexReactClient } from "convex/react";
 
 import {
-  createRootRouteWithContext,
-  useRouteContext,
+	createRootRouteWithContext,
+	useRouteContext,
 } from "@tanstack/react-router";
 import { Outlet, HeadContent, Scripts } from "@tanstack/react-router";
 import { createServerFn } from "@tanstack/react-start";
@@ -13,111 +13,119 @@ import { ClerkProvider, useAuth } from "@clerk/tanstack-start";
 import { getAuth } from "@clerk/tanstack-start/server";
 import { getWebRequest } from "@tanstack/react-start/server";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { Toaster } from "@/components/ui/sonner";
 
 import { useTheme } from "@/hooks/use-theme";
 
-import "../main.css";
+import appCss from "@/main.css?url";
 
 const CLERK_PUBLISHABLE_KEY = (import.meta.env as Record<string, string>)
-  .VITE_CLERK_PUBLISHABLE_KEY;
+	.VITE_CLERK_PUBLISHABLE_KEY;
 
 const fetchClerkAuth = createServerFn({ method: "GET" }).handler(async () => {
-  try {
-    const request = getWebRequest();
-    if (!request) {
-      throw new Error("No web request found");
-    }
-    const auth = await getAuth(request);
-    const token = await auth.getToken({ template: "convex" });
+	try {
+		const request = getWebRequest();
+		if (!request) {
+			throw new Error("No web request found");
+		}
+		const auth = await getAuth(request);
+		const token = await auth.getToken({ template: "convex" });
 
-    return {
-      userId: auth.userId,
-      token,
-    };
-  } catch (e) {
-    console.error("Auth error:", e);
-    return {
-      userId: null,
-      token: null,
-    };
-  }
+		return {
+			userId: auth.userId,
+			token,
+		};
+	} catch (e) {
+		console.error("Auth error:", e);
+		return {
+			userId: null,
+			token: null,
+		};
+	}
 });
 
 export const Route = createRootRouteWithContext<{
-  queryClient: QueryClient;
-  convexClient: ConvexReactClient;
-  convexQueryClient: ConvexQueryClient;
+	queryClient: QueryClient;
+	convexClient: ConvexReactClient;
+	convexQueryClient: ConvexQueryClient;
 }>()({
-  head: () => ({
-    meta: [
-      {
-        charSet: "utf-8",
-      },
-      {
-        name: "viewport",
-        content: "width=device-width, initial-scale=1",
-      },
-      {
-        title: "builder - the easiest way to build your landing page",
-      },
-    ],
-  }),
-  beforeLoad: async (ctx) => {
-    try {
-      const auth = await fetchClerkAuth();
-      const { userId, token } = auth;
+	head: () => ({
+		meta: [
+			{
+				charSet: "utf-8",
+			},
+			{
+				name: "viewport",
+				content: "width=device-width, initial-scale=1",
+			},
+			{
+				title: "builder - the easiest way to build your landing page",
+			},
+		],
+		links: [
+			{
+				rel: "stylesheet",
+				href: appCss,
+			},
+		],
+	}),
+	beforeLoad: async (ctx) => {
+		try {
+			const auth = await fetchClerkAuth();
+			const { userId, token } = auth;
 
-      // During SSR only (the only time serverHttpClient exists),
-      // set the Clerk auth token to make HTTP queries with.
-      if (token) {
-        ctx.context.convexQueryClient.serverHttpClient?.setAuth(token);
-      }
+			// During SSR only (the only time serverHttpClient exists),
+			// set the Clerk auth token to make HTTP queries with.
+			if (token) {
+				ctx.context.convexQueryClient.serverHttpClient?.setAuth(token);
+			}
 
-      return {
-        userId,
-        token,
-      };
-    } catch (error) {
-      console.error("Auth error:", error);
-      return {
-        userId: null,
-        token: null,
-      };
-    }
-  },
-  errorComponent: ({ error }) => {
-    return <div>Error: {error.message}</div>;
-  },
-  notFoundComponent: () => <div>Not found</div>,
-  component: RootComponent,
+			return {
+				userId,
+				token,
+			};
+		} catch (error) {
+			console.error("Auth error:", error);
+			return {
+				userId: null,
+				token: null,
+			};
+		}
+	},
+	errorComponent: ({ error }) => {
+		return <div>Error: {error.message}</div>;
+	},
+	notFoundComponent: () => <div>Not found</div>,
+	component: RootComponent,
 });
 
 function RootComponent() {
-  const context = useRouteContext({ from: Route.id });
+	const context = useRouteContext({ from: Route.id });
 
-  return (
-    <ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY}>
-      <ConvexProviderWithClerk client={context.convexClient} useAuth={useAuth}>
-        <RootDocument>
-          <Outlet />
-        </RootDocument>
-      </ConvexProviderWithClerk>
-    </ClerkProvider>
-  );
+	return (
+		<ClerkProvider publishableKey={CLERK_PUBLISHABLE_KEY}>
+			<ConvexProviderWithClerk client={context.convexClient} useAuth={useAuth}>
+				<RootDocument>
+					<Outlet />
+				</RootDocument>
+			</ConvexProviderWithClerk>
+		</ClerkProvider>
+	);
 }
 
 function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
-  const { theme } = useTheme();
+	const { theme } = useTheme();
 
-  return (
-    <html lang="en" className={theme}>
-      <head>
-        <HeadContent />
-      </head>
-      <body>
-        {children}
-        <Scripts />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" className={theme}>
+			<head>
+				<HeadContent />
+			</head>
+			<body>
+				{children}
+				<Scripts />
+				<Toaster />
+			</body>
+		</html>
+	);
 }
