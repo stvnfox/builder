4be14c9a7f957fbd47c538e841a8453fbbcf2794
steven.vos <steven.vos@iodigital.com>
2025-03-31 15:45:29 +0200

    feat(sign-in): redirect to dashboard after sign in

--- app/routes/sign-in.tsx
+++ app/routes/sign-in.tsx
@@ -4,16 +4,18 @@ import { SignIn } from "@clerk/tanstack-start";
 export const Route = createFileRoute("/sign-in")({
 	beforeLoad: ({ context }) => {
 		if (context.userId) {
-			throw redirect({ to: "/" });
+			throw redirect({ to: "/dashboard" });
 		}
 	},
 	component: RouteComponent,
 });
 
 function RouteComponent() {
+	const dashboardUrl = `${window.location.origin}/dashboard`;
+
 	return (
 		<div className="flex min-h-screen min-w-screen items-center justify-center">
-			<SignIn routing="hash" forceRedirectUrl={window.location.href} />
+			<SignIn routing="hash" forceRedirectUrl={dashboardUrl} />
 		</div>
 	);
 }
