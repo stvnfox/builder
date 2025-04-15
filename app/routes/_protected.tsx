8b60243d901b3c0967b84f321ce08a88ac0efc5a
steven.vos <steven.vos@iodigital.com>
2025-04-15 11:34:29 +0200

    feat(get pages): add get pages loader

--- app/routes/_protected.tsx
+++ app/routes/_protected.tsx
@@ -1,5 +1,8 @@
 import { Outlet, createFileRoute, redirect } from "@tanstack/react-router";
 
+import { convexQuery } from "@convex-dev/react-query";
+import { api } from "@convex/_generated/api";
+
 import { ViewProvider } from "@/providers/view-provider";
 
 import { AppSidebar } from "@/components/layout/app-sidebar";
@@ -15,6 +18,13 @@ export const Route = createFileRoute("/_protected")({
 		if (!context.userId) {
 			throw redirect({ to: "/sign-in" });
 		}
+	},
+	loader: async (opts) => {
+		await opts.context.queryClient.ensureQueryData(
+			convexQuery(api.pages.getPagesByUserId, {
+				userId: opts.context.userId as string,
+			}),
+		);
 	},
 	errorComponent: ({ error }) => {
 		throw error;
